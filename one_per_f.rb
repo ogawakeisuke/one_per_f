73d746257383199ccdeb435f44a12960ed7b31a5
ogawakeisuke <ogawa@uniba.jp>
2013-07-30 15:27:38 +0900

    コメント追記

--- one_per_f.rb
+++ one_per_f.rb
@@ -15,7 +15,7 @@ RubyAudio::Sound.open(fname) do |snd|
   while snd.read(buf) != 0
     na = NArray.to_na(buf.to_a)
 
-    fft_slice = FFTW3.fft(na, -1).to_a[0, window_size / 2].collect { |complex| complex.abs * 10 / (window_size / 2) }
+    fft_slice = FFTW3.fft(na, -1).to_a[0, window_size / 2].collect { |complex| Math.log10(complex.abs / (window_size / 2) )}
     
     results << RegLine.new(fft_slice).regression_slope
   end
@@ -26,4 +26,4 @@ p (results.inject { |sum, x| sum += x } / results.size)
 
 
 
-# -1.0に近づけば高評価、だと思う
+# -0.0001に近づけば高評価、だと思う
